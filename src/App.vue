724579736b77b22aeefc7f7684ecd64b82d78101
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-03 16:43:48 +0400

    added main vue file App.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+ <template>
+ <h2>My Friends</h2>
+ <section>
+     <ul>
+         <li></li>
+     </ul>
+ </section>
+ </template>
+
+<script>
+export default {
+  data() {
+      return {
+          friends: [
+              {
+                  id: 'manuel',
+                  name: 'Manuel Lorenz',
+                  phone: '+994 50 270 25 69',
+                  email: 'manuel@localhost.com'
+              },
+              {
+                  id: 'julie',
+                  name: 'Julie Fazzy',
+                  phone: '+994 55 270 25 69',
+                  email: 'julie@localhost.com'
+              }
+          ]
+      }
+  }
+};
+</script>
