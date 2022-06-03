d3173399208ad75316082bf0f4b105650926d102
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-03 17:12:20 +0400

    add friend-contact component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
  <template>
- <h2>My Friends</h2>
- <section>
-     <ul>
-         <li></li>
-     </ul>
- </section>
- </template>
+  <h2>My Friends</h2>
+  <section>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
 
 <script>
+
 export default {
   data() {
-      return {
-          friends: [
-              {
-                  id: 'manuel',
-                  name: 'Manuel Lorenz',
-                  phone: '+994 50 270 25 69',
-                  email: 'manuel@localhost.com'
-              },
-              {
-                  id: 'julie',
-                  name: 'Julie Fazzy',
-                  phone: '+994 55 270 25 69',
-                  email: 'julie@localhost.com'
-              }
-          ]
-      }
-  }
+    return {
+      FriendContact: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "+994 50 270 25 69",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Fazzy",
+          phone: "+994 55 270 25 69",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
 };
 </script>
